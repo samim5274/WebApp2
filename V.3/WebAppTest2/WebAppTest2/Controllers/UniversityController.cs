7bed0b2acd550940bc18487f3ede1e87ad8d2b49
samim5274 <banglarcele1122@gmail.com>
2023-04-30 23:36:12 +0600

    update

---
+++ V.3/WebAppTest2/WebAppTest2/Controllers/UniversityController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTest2.Controllers
+{
+    public class UniversityController : Controller
+    {
+        // GET: University
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dept()
+        {
+            string[] DeptList = { "CSE", "EEE", "BBA","DBA", "English" };
+            ViewBag.dept = DeptList;
+            return View();
+        }
+
+        public ActionResult Course()
+        {
+            string[] courseList = { "Matrix and Algebra", "Neumerical Stadis","Data Structure","C# Programming","Database Manegment"};
+            ViewBag.list = courseList;
+            return View();
+        }
+
+        public ActionResult Degree()
+        {
+            return View();
+        }
+
+        public ActionResult Programs()
+        {
+            return RedirectToAction("Dept");
+        }
+    }
+}
