3ba1bbbaf90d3388474e3a0ffba2c159f0d00ecb
samim5274 <banglarcele1122@gmail.com>
2023-05-02 01:27:08 +0600

    update

---
+++ V.4/WebAppTest2/WebAppTest2/Controllers/TedtController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppTest2.Models;
+
+namespace WebAppTest2.Controllers
+{
+    public class TedtController : Controller
+    {
+        // GET: Tedt
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegFrom()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveData(string stdName, int stdAge, string stdNationality)
+        {
+            //return Content($"Name: {stdName}, Age: {stdAge}, Nationality: {stdNationality}");
+
+            ViewBag.name = stdName;
+            ViewBag.age = stdAge;
+            ViewBag.national = stdNationality;
+
+            return View();
+        }
+
+        public ActionResult CourseRegistationForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registation(Student std)
+        {
+            return View("ShowData", std);
+        }
+    }
+}
---
+++ V.4/WebAppTest2/WebAppTest2/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTest2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public int RollNo { get; set; }
+        public String Department { get; set; }
+        public string Session { get; set; }
+    }
+}
